b2a9f9eadd0a4898c50b9f989da8954a387a8d2e
Jasmine Hindson <jasmine@swipedon.com>
2019-09-02 17:32:39 +1200

    added a new column to database that will hold the translated value and changed the language text field to only hold 5 values.

--- www/convert-language/converter.php
+++ www/convert-language/converter.php
@@ -1,61 +1,13 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
-<head><title>Database test page</title>
-<style>
-th { text-align: left; }
-
-table, th, td {
-  border: 2px solid grey;
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 0.2em;
-}
-</style>
-</head>
-
 <body>
-<h1>Database test page</h1>
-
-<p>Showing contents of WordConverter table:</p>
-<p><button class="btnConvert">Convert Word to Language</button></p>
-<br><table border="1">
-<tr><th>Word</th><th>Language</th></tr>
 
 <?php
- 
-$db_host   = '192.168.2.31';
-$db_name   = 'fvision';
-$db_user   = 'webuser';
-$db_passwd = 'insecure_db_pw';
+   function translate($from_lan, $to_lan, $text){
+        $json = json_decode(file_get_contents('https://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=' . urlencode($text) . '&langpair=' . $from_lan . '|' . $to_lan));
+        $translated_text = $json->responseData->translatedText;
 
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-
-$q = $pdo->query("SELECT * FROM WordConverter");
-
-
-while($row = $q->fetch()){
-  echo "<tr><td>".$row["word"]."</td><td>".$row["lang"]."</td></tr>\n";
-}
-    
+        return $translated_text;
+    }
 ?>
-
-</table></br>
-
-<p>Add a new word to the table:</p>
-<form action="insert.php" method="post">
-    <p>
-        <label for="word">Word:</label>
-        <input type="text" name="word" id="word">
-    </p>
-    <p>
-        <label for="language">Language:</label>
-        <input type="text" name="language" id="language">
-    </p>
-    <input type="submit" value="Submit">
-</form>
 </body>
 </html>
--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
-<head><title>Database test page</title>
+<head><title>Language Converter</title>
 <style>
 th { text-align: left; }
 
@@ -18,32 +18,40 @@ th, td {
 <body>
 <h1>Database test page</h1>
 
-<p>Showing contents of WordConverter table:</p>
-<p><button class="btnConvert">Convert Word to Language</button></p>
+<p>Translated Words Below:</p>
 <br><table border="1">
-<tr><th>Word</th><th>Language</th></tr>
+<tr><th>Word in English</th><th>Language</th><th>Translated Word</th></tr>
 
 <?php
- 
-$db_host   = '192.168.2.31';
-$db_name   = 'fvision';
-$db_user   = 'webuser';
-$db_passwd = 'insecure_db_pw';
+    
+    $language = $_POST['language'];
+    $db_host   = '192.168.2.31';
+    $db_name   = 'fvision';
+    $db_user   = 'webuser';
+    $db_passwd = 'insecure_db_pw';
 
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+    $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+    $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
-$q = $pdo->query("SELECT * FROM WordConverter");
+    $q = $pdo->query("SELECT * FROM WordConverter");
 
 
-while($row = $q->fetch()){
-  echo "<tr><td>".$row["word"]."</td><td>".$row["lang"]."</td></tr>\n";
-}
+    while($row = $q->fetch()){
+        echo "<tr><td>".$row["word"]."</td><td>".$row["lang"]."</td><td>".$row["translated_word"]."</td></tr>\n";
+    }
     
 ?>
 
 </table></br>
+
+<?php
+    function getSelectSession($val, $val2) {
+       if ($_SESSION[$val] == $val2) {
+           echo "selected";
+       }
+    }
+?>
 
 <p>Add a new word to the table:</p>
 <form action="insert.php" method="post">
@@ -51,11 +59,17 @@ while($row = $q->fetch()){
         <label for="word">Word:</label>
         <input type="text" name="word" id="word">
     </p>
-    <p>
-        <label for="language">Language:</label>
-        <input type="text" name="language" id="language">
-    </p>
+     <p> Translate from English to:
+           <select name="language">
+             <option value="french" <?php getSelectSession("lang", "french"); ?>>French</option>
+             <option value="italian" <?php getSelectSession("lang", "italian"); ?>>Italian</option>
+             <option value="spanish" <?php getSelectSession("lang", "spanish"); ?>>Spanish</option>
+             <option value="portugese"<?php getSelectSession("lang", "portugese"); ?>>Portugese</option>
+           </select>
+</p>
     <input type="submit" value="Submit">
 </form>
+
+
 </body>
 </html>
