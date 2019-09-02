cece82b4a9eff68df3980c75572a31f3a0d836a0
Jasmine Hindson <jasmine@swipedon.com>
2019-09-02 16:20:27 +1200

    created convert-website.conf for the convertserver VM and changed this in the Vagrantfile as well.

--- Vagrantfile
+++ Vagrantfile
@@ -21,10 +21,10 @@ Vagrant.configure("2") do |config|
       apt-get install -y apache2 php libapache2-mod-php php-mysql
             
       # Change VM's webserver's configuration to use shared folder.
-      # (Look inside test-website.conf for specifics.)
-      cp /vagrant/test-website.conf /etc/apache2/sites-available/
+      # (Look inside main-website.conf for specifics.)
+      cp /vagrant/main-website.conf /etc/apache2/sites-available/
       # activate our website configuration ...
-      a2ensite test-website
+      a2ensite main-website
       # ... and disable the default website provided with Apache
       a2dissite 000-default
       # Reload the webserver configuration, to pick up our changes
@@ -70,8 +70,8 @@ Vagrant.configure("2") do |config|
       apt-get update
       apt-get install -y apache2 php libapache2-mod-php php-mysql
 
-      cp /vagrant/test-website.conf /etc/apache2/sites-available/
-      a2ensite test-website
+      cp /vagrant/convert-website.conf /etc/apache2/sites-available/
+      a2ensite convert-website
       a2dissite 000-default
       service apache2 reload
     SHELL
---
+++ www/converter.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head><title>Database test page</title>
+<style>
+th { text-align: left; }
+
+table, th, td {
+  border: 2px solid grey;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 0.2em;
+}
+</style>
+</head>
+
+<body>
+<h1>Database test page</h1>
+
+<p>Showing contents of WordConverter table:</p>
+<p><button class="btnConvert">Convert Word to Language</button></p>
+<br><table border="1">
+<tr><th>Word</th><th>Language</th></tr>
+
+<?php
+ 
+$db_host   = '192.168.2.31';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+$q = $pdo->query("SELECT * FROM WordConverter");
+
+
+while($row = $q->fetch()){
+  echo "<tr><td>".$row["word"]."</td><td>".$row["lang"]."</td></tr>\n";
+}
+    
+?>
+
+</table></br>
+
+<p>Add a new word to the table:</p>
+<form action="insert.php" method="post">
+    <p>
+        <label for="word">Word:</label>
+        <input type="text" name="word" id="word">
+    </p>
+    <p>
+        <label for="language">Language:</label>
+        <input type="text" name="language" id="language">
+    </p>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
